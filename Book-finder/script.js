1fce98c9b42bb2aa3a6cda15c8d8db878f6aebaa
Italo Andrey Motta de Andrade <79292620+Italo-Andrey@users.noreply.github.com>
2021-07-09 14:56:49 -0300

    Update script.js

--- Book-finder/script.js
+++ Book-finder/script.js
@@ -1,3 +1,6 @@
 const key = 'AIzaSyCasjCpCfx6djnRA5Nvpyab3-YNXi2z58M';
-const url = ;
+const url = 'https://www.googleapis.com/books/v1';
 
+let input = document.querySelector("#input")
+
+console.log(input.value)
